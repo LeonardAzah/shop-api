c2f5ec752ad32c65ae3e8d22313a0cca626d1af7
LeonardAzah <isongleonard@gmail.com>
2024-06-03 19:14:24 +0100

    apply validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,22 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsPhoneNumber,
+  IsString,
+  Length,
+} from 'class-validator';
+
 export class CreateUserDto {
+  @Length(2, 50)
   name: string;
+
+  @IsEmail()
   email: string;
+
+  @IsPhoneNumber('CM')
   phone: string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,24 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsPhoneNumber,
+  IsString,
+  Length,
+} from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @Length(2, 50)
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsPhoneNumber('CM')
+  phone: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
