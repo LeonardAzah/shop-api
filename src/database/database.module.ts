527fca507ba089c7a58f1d4fa77e21aa6d324b42
LeonardAzah <isongleonard@gmail.com>
2024-06-03 20:24:35 +0100

    database config

---
+++ src/database/database.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      username: 'root',
+      password: 'lamar',
+      host: 'localhost',
+      port: 3306,
+      database: 'shop_db',
+      //   synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ],
+})
+export class DatabaseModule {}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,7 +1,23 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { RegistryDates } from '../../common/embedded/registry-dates.embedded';
+
+@Entity()
 export class User {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column()
   name: string;
+
+  @Column({ unique: true })
   email: string;
+
+  @Column({ unique: true })
   phone: string;
+
+  @Column()
   password: string;
+
+  @Column(() => RegistryDates, { prefix: false })
+  registryDates: RegistryDates;
 }
